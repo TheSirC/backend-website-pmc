aef1b807bf12ccfea380407959422c1e1de4a588
TheSirC <clocloranely@hotmail.fr>
2020-08-25 09:07:39 +0200

    Added localisation in database query

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,11 @@
 use crate::paste;
 use crate::diesel::PgConnection;
 use crate::diesel::RunQueryDsl;
+use crate::diesel::QueryDsl;
+use crate::diesel::Queryable;
+use crate::serde::Serialize;
 use crate::schema;
+use crate::models::localisations;
 
 #[database("stock_db")]
 pub struct MyPgDatabase(PgConnection);
@@ -10,10 +14,17 @@ macro_rules! get_equipment {
     ($($equipment:ident),+) => {
         paste::item! {
         $(
+            #[derive(Serialize, Queryable)]
+            pub struct [<$equipment _location>] {
+                equipment: $equipment,
+                location: localisations
+            }
             use crate::models::$equipment;
-            pub fn [<get_ $equipment>](db_connection: &PgConnection) -> Vec<$equipment> {
-                schema::$equipment::table
-                    .load::<$equipment>(db_connection)
+            pub fn [<get_ $equipment>](db_connection: &PgConnection) -> Vec<[<$equipment _location>]> {
+                schema::$equipment::table.inner_join(
+                    schema::localisations::table
+                )
+                    .load(db_connection)
                     .expect("The call to the database for the equipment table did not go well")
             }
         )+
