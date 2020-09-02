6e537bccc103e4302d89cf02544feebfa51c2fae
TheSirC <clocloranely@hotmail.fr>
2020-09-02 14:29:09 +0200

    Cleaning up the nix-shell
    Changes were cosmetic. This file is only there for development purposes.

--- shell.nix
+++ shell.nix
@@ -1,14 +1,9 @@
 {pkgs ? import <nixpkgs> {}}:
 
 with pkgs;
-let
+mkShell {
+  buildInputs = [ pgcli postgresql_11 inotify-tools diesel-cli ];
   # Just a way to have the bootstrapping script standalone (with syntax highlighting)
   shellHook = builtins.readFile ./.shell.sh;
-in
-mkShell {
-  buildInputs = [ pgcli postgresql_11 inotify-tools diesel-cli];
-  shellHook = ''
-            ${shellHook}
-  '';
   LOCALE_ARCHIVE = if pkgs.stdenv.isLinux then "${pkgs.glibcLocales}/lib/locale/locale-archive" else "";
 }
